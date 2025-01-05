760c53708b68f8aa791e132dc5ca09f93d78f36a
Charles Owuala <51397595+CHARLE123455@users.noreply.github.com>
2025-01-05 12:42:17 +0100

    debugging log routes

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -1,7 +1,7 @@
-const exports = require('express');
+const express = require('express');
 const LogController = require('../controllers/logController');
 const {authenticateJWT} = require('../middleware/authMiddleware');
-const router = exports.Router();
+const router = express.Router();
 
 router.get('/actions', authenticateJWT, LogController.getAllActions);
 
