8ffec0cc5cceb4520aa77000309edbd8a6419745
Charles Owuala <51397595+CHARLE123455@users.noreply.github.com>
2025-01-07 19:51:10 +0100

    changes made to log route

--- src/routes/logRoutes.js
+++ src/routes/logRoutes.js
@@ -3,8 +3,7 @@ const LogController = require('../controllers/logController');
 const {authenticateJWT} = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get('/actions',  LogController.getAllLogsByAction);
-router.get('/all', LogController.getAllLogs);
+router.get('/actions/:action',  LogController.getAllLogsByAction);
 router.post('/create', LogController.createLog);
 
 module.exports = router;
