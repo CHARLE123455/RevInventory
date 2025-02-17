0b0343804c1ae4591d9a08b39df16d84e17f1173
Charles Owuala <51397595+CHARLE123455@users.noreply.github.com>
2025-02-17 14:00:31 +0100

    `Refactored itemService.js: updated imports, added try-catch blocks, and removed unused code`

--- src/services/itemService.js
+++ src/services/itemService.js
@@ -1,39 +1,43 @@
-const Item = require('../models/Item');
-const Log = require('../models/Log');
+const Item = require("../models/Item");
 
 exports.createItem = async (data, imageUrl) => {
-    try {
-        const { name, price, quantity, categoryId } = data;
+  try {
+    const { name, price, quantity, categoryId } = data;
 
-        // Create item with imageUrl
-        const item = await Item.create({
-            name,
-            price,
-            quantity,
-            categoryId,
-            imageUrl: imageUrl || null
-        });
-        return item;
-    } catch (error) {
-        throw new Error(`Failed to create item: ${error.message}`);
-    }
+    // Create item with imageUrl
+    const item = await Item.create({
+      name,
+      price,
+      quantity,
+      categoryId,
+      imageUrl: imageUrl || null,
+    });
+    return item;
+  } catch (error) {
+    throw new Error(`Failed to create item: ${error.message}`);
+  }
 };
 
 exports.updateItemQuantity = async (id, quantity) => {
+  try {
     const item = await Item.findByPk(id);
     if (!item) throw new Error("Item not found");
-    
-    item.quantity = quantity;
-    await item.save();  
-    return item;
-};
 
-exports.sellItem = async (id, quantitySold, buyer) => {
-    const item = await Item.findByPk(id);
-    if (!item) throw new Error("Item not found");
-    if (item.quantity < quantitySold) throw new Error("Insufficient quantity, kindly restock!");
-    
-    item.quantity -= quantitySold;
+    item.quantity = quantity;
     await item.save();
     return item;
+  } catch (errors) {
+    throw new Error(`Failed to update item quantity: ${errors.message}`);
+  }
+};
+
+exports.sellItem = async (id, quantitySold) => {
+  const item = await Item.findByPk(id);
+  if (!item) throw new Error("Item not found");
+  if (item.quantity < quantitySold)
+    throw new Error("Insufficient quantity, kindly restock!");
+
+  item.quantity -= quantitySold;
+  await item.save();
+  return item;
 };
